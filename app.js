2797ec83c9fb9758e7856422cc7cf78427848b1c
Soham Dave <dave.soham2000@gmail.com>
2023-02-15 15:57:31 -0800

    POST Request done

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ let medicalData = [];
 
 app.get('/items', (req, res) => {
     res.json(medicalData);
-  });
+});
+
+app.post('/items', (req, res) => {
+    const data = {
+      patient_name: req.body.patient_name,
+      patient_address: req.body.patient_address,
+      hospital_name: req.body.hospital_name,
+      date_of_service: req.body.date_of_service,
+      bill_amount: req.body.bill_amount
+    };
+    medicalData.push(data);
+    res.status(201).json(data);
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
